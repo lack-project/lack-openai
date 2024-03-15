157c37ab1e5d9a0225625ea4039cad04939d8e10
Matthias Leuffen <matthes@leuffen.de>
2024-03-15 15:37:24 +0100

    sync

--- src/Helper/JobTemplate.php
+++ src/Helper/JobTemplate.php
@@ -41,17 +41,49 @@ class JobTemplate
      * @return string
      */
     private function _parseSection(string $template, string $debugSectionName) : string {
-
-        return preg_replace_callback("/\{{([a-zA-Z0-9_]+)\}\}/", function ($matches) use($debugSectionName) {
+        // Parse {% ifdef key %}...{% endif %} and remove if key is not set (orherwise return the inner section without the if)
+        $template = preg_replace_callback("/\{%\s*ifdef\s+([a-zA-Z0-9_]+)\s*%\}(.+?)\{%\s*endif\s*%\}/s", function ($matches) use($debugSectionName) {
             $key = $matches[1];
-            if (isset ($this->data[$key])) {
-                return $this->data[$key];
+            if (isset ($this->data[$key]) && $this->data[$key] !== null) {
+                return $matches[2];
             }
-            if ($this->dataLoader !== null) {
-                return ($this->dataLoader)($key);
+            if ($this->dataLoader !== null && ($this->dataLoader)($key) !== null) {
+                return $matches[2];
             }
-            throw new \InvalidArgumentException("Key '$key' not found in template. (Section: $debugSectionName in file: {$this->templateFileName})");
+            return ""; // Skip section if not found
         }, $template);
+        
+        
+        // Replace all {{key}} with value from data
+        // Allows to use {{ key | htmlentitys }} to apply a filter
+        
+        $template =  preg_replace_callback("/\{{([a-zA-Z0-9_| ]+)\}\}/", function ($matches) use($debugSectionName) {
+            // Split by pipe
+            $parts = explode("|", $matches[1]);
+            $key = trim (array_shift($parts));
+            $value = null;
+            if (isset ($this->data[$key])) {
+                $value = $this->data[$key];
+            } else if ($this->dataLoader !== null) {
+                $value = ($this->dataLoader)($key);
+            } else {
+                throw new \InvalidArgumentException("Key '$key' not found in template. (Section: $debugSectionName in file: {$this->templateFileName})");
+            }
+           
+            // Filter
+            foreach ($parts as $filter) {
+                $filter = trim($filter);
+                if ($filter == "htmlentities") {
+                    $value = htmlentities($value);
+                } else {
+                    throw new \InvalidArgumentException("Unknown filter '$filter' in template. (Section: $debugSectionName in file: {$this->templateFileName})");
+                }
+            }
+            
+            return $value;
+            
+        }, $template);
+        return $template;
 
     }
 
